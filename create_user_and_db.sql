0de4a7376fdffb64bafc314d0382deeabc2a2fa2
rebelscum <linht@vulcan.com>
2015-07-13 14:07:45 -0700

    Updated to add the distribution_int user for the use exclusively by the taxon distribution tool.

--- create_user_and_db.sql
+++ create_user_and_db.sql
@@ -11,6 +11,8 @@ DROP USER IF EXISTS web_int;
 CREATE USER web_int WITH PASSWORD 'web_int';
 DROP USER IF EXISTS recon_int;
 CREATE USER recon_int WITH PASSWORD 'recon_int';
+DROP USER IF EXISTS distribution_int;
+CREATE USER distribution_int WITH PASSWORD 'distribution_int';
 
 ALTER DATABASE sau_int OWNER TO sau_int;
 GRANT postgres TO sau_int;
@@ -18,3 +20,4 @@ GRANT postgres TO sau_int;
 ALTER USER sau_int SET search_path TO admin, master, recon, distribution, log, tiger, topology, tiger_data, public;
 ALTER USER web_int SET search_path TO master, recon, distribution, log, admin, tiger, topology, tiger_data, public;
 ALTER USER recon_int SET search_path TO recon, log, master, admin, distribution, tiger, topology, tiger_data, public;
+ALTER USER distribution_int SET search_path TO distribution, master, admin, recon, log, tiger, topology, tiger_data, public;
--- grant.sql
+++ grant.sql
@@ -47,3 +47,29 @@ GRANT USAGE ON SCHEMA recon TO recon_int;
 GRANT ALL ON ALL TABLES IN SCHEMA recon TO recon_int;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA recon TO recon_int;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO recon_int;
+
+-- For user distribution_int
+GRANT USAGE ON SCHEMA admin TO distribution_int;
+GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA admin TO distribution_int;
+GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA admin TO distribution_int;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA admin TO distribution_int;
+
+GRANT USAGE ON SCHEMA master TO distribution_int;
+GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA master TO distribution_int;
+GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA master TO distribution_int;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA master TO distribution_int;
+
+GRANT USAGE ON SCHEMA log TO distribution_int;
+GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO distribution_int;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA log TO distribution_int;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO distribution_int;
+
+GRANT USAGE ON SCHEMA distribution TO distribution_int;
+GRANT ALL ON ALL TABLES IN SCHEMA distribution TO distribution_int;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA distribution TO distribution_int;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA distribution TO distribution_int;
+
+GRANT USAGE ON SCHEMA recon TO distribution_int;
+GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA recon TO distribution_int;
+GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA recon TO distribution_int;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO distribution_int;
