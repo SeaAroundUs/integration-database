7db79b65e9b81e6547592fee46b3c1b8ba796518
Rebel Scum <linht@vulcan.com>
2015-10-09 11:03:12 -0700

    Updated to add the qc_int user for UBC staff.

--- create_user_and_db.sql
+++ create_user_and_db.sql
@@ -13,6 +13,8 @@ DROP USER IF EXISTS recon_int;
 CREATE USER recon_int WITH PASSWORD 'recon_int';
 DROP USER IF EXISTS distribution_int;
 CREATE USER distribution_int WITH PASSWORD 'distribution_int';
+DROP USER IF EXISTS qc_int;
+CREATE USER qc_int WITH PASSWORD 'qc_int';
 
 ALTER DATABASE sau_int OWNER TO sau_int;
 GRANT postgres TO sau_int;
@@ -21,3 +23,4 @@ ALTER USER sau_int SET search_path TO admin, master, recon, distribution, log, t
 ALTER USER web_int SET search_path TO master, recon, distribution, log, admin, tiger, topology, tiger_data, public;
 ALTER USER recon_int SET search_path TO recon, log, master, admin, distribution, tiger, topology, tiger_data, public;
 ALTER USER distribution_int SET search_path TO distribution, master, admin, recon, log, tiger, topology, tiger_data, public;
+ALTER USER qc_int SET search_path TO master, recon, distribution, log, admin, public;
