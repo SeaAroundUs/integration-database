fcdffda2243f0f28d86ac8a89ec9d38f9bddb789
Rebel Scum <linht@vulcan.com>
2015-10-26 11:19:20 -0700

    Added the allocation.ices_area table to be the source for Merlin.

--- table_allocation.sql
+++ table_allocation.sql
@@ -2,3 +2,9 @@ CREATE TABLE allocation.ifa(
   eez_id int,
   ifa_is_located_in_this_fao int
 );
+
+CREATE TABLE allocation.ices_area(  
+  ices_division varchar(255),
+  ices_subdivision varchar(255),
+  ices_area_id varchar(255) not null  
+);
