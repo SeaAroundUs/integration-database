f622db5634b26ca98171b3a674bc3e6ae159da45
Robert Reis <T-RobertR@vulcan.com>
2015-11-16 11:13:35 -0800

    health of the catch table views

--- view_recon.sql
+++ view_recon.sql
@@ -1,5 +1,5 @@
 --
--- warnings (non-blocking)
+-- raw_catch warnings (non-blocking)
 --
    
 -- Year greater than 2010   
@@ -47,7 +47,7 @@ CREATE OR REPLACE VIEW recon.v_raw_catch_taxa_is_rare AS
 SELECT rc.id FROM raw_catch rc, rare_taxon rt WHERE rc.taxon_key = rt.taxon_key;
 
 --
--- errors (blocking)
+-- raw_catch errors (blocking)
 --
 
 -- Fishing entity and EEZ rule for Layer
@@ -119,3 +119,88 @@ CREATE OR REPLACE VIEW recon.v_raw_catch_missing_required_field AS
 SELECT id                
   FROM raw_catch 
  WHERE (fishing_entity || eez || fao_area || layer || sector || catch_type || "year" || taxon_name || amount || input_type) IS NULL;
+
+--
+-- catch warnings
+--
+
+-- Year greater than 2010
+CREATE OR REPLACE VIEW recon.v_catch_year_greater_2010 AS
+  SELECT id FROM catch WHERE year > 2010;
+
+-- Original taxon name is not null
+CREATE OR REPLACE VIEW recon.v_catch_original_taxon_not_null  AS
+  SELECT id FROM catch WHERE original_taxon_name_id IS NOT NULL;
+
+-- Original country fishing is not null
+CREATE OR REPLACE VIEW recon.v_catch_original_country_fishing_not_null AS
+  SELECT id FROM catch WHERE original_country_fishing_id IS NOT NULL;
+
+-- Original sector is not null
+CREATE OR REPLACE VIEW recon.v_catch_original_sector_not_null AS
+  SELECT id FROM catch WHERE original_sector_id IS NOT NULL;
+
+-- Catch amount greater than 15e6 for Peru
+CREATE OR REPLACE VIEW recon.v_catch_peru_catch_amount_greater_than_threshold AS
+  SELECT id FROM catch WHERE amount > 15e6 AND eez_id = 604;
+
+-- Catch amount greater than 5e6 for others
+CREATE OR REPLACE VIEW recon.v_catch_amount_greater_than_threshold AS
+  SELECT id FROM catch WHERE amount > 5e6 AND eez_id != 604;
+
+-- FAO is 27 and ICES is null
+CREATE OR REPLACE VIEW recon.v_catch_fao_27_ices_null AS
+  SELECT id FROM catch WHERE fao_area_id = 27 AND ices_area_id IS NULL;
+
+-- FAO is 21 and NAFO is null
+CREATE OR REPLACE VIEW recon.v_catch_fao_21_nafo_null AS
+  SELECT id FROM catch WHERE fao_area_id = 21 AND nafo_division_id IS NULL;
+
+-- Sector is subsistence and Layer is not 1
+CREATE OR REPLACE VIEW recon.v_catch_subsistence_and_layer_not_1 AS
+  SELECT id FROM catch WHERE sector_type_id = 2 AND layer != 1;
+
+-- Layer is 2 or 3 and sector is not industrial
+CREATE OR REPLACE VIEW recon.v_catch_layer_2_or_3_and_sector_not_industrial AS
+  SELECT id FROM catch WHERE layer IN (2,3) AND sector_type_id != 1;
+
+-- Rare taxa should be excluded
+CREATE OR REPLACE VIEW recon.v_catch_taxa_is_rare AS
+  SELECT c.id FROM catch c, rare_taxon rt WHERE c.taxon_key = rt.taxon_key;
+
+--
+-- catch errors
+--
+
+CREATE OR REPLACE VIEW recon.v_catch_fishing_entity_and_eez_not_aligned AS
+  WITH eez_fishing_entity AS (
+      SELECT eez.eez_id, eez.is_home_eez_of_fishing_entity_id AS expected_fishing_entity FROM eez
+  )
+  SELECT c.id
+  FROM catch c
+    LEFT JOIN eez_fishing_entity efe ON (c.eez_id = efe.eez_id)
+    LEFT JOIN layer3_taxon l3 ON (l3.taxon_key = c.taxon_key)
+  WHERE ((c.eez_id <> 0 AND c.fishing_entity_id <> 0) OR l3.taxon_key IS NOT NULL)
+        AND c.layer
+            IS DISTINCT FROM
+            CASE WHEN l3.taxon_key IS NOT NULL THEN 3
+            WHEN c.fishing_entity_id IS NOT DISTINCT FROM efe.expected_fishing_entity THEN 1
+            ELSE 2
+            END
+;
+
+-- Input type is reconstructed and Catch type is reported landings
+CREATE OR REPLACE VIEW recon.v_catch_input_reconstructed_catch_type_reported AS
+  SELECT id FROM catch WHERE input_type_id = 1 AND catch_type_id = 1;
+
+-- Input type is not reconstructed and Catch type not reported landings
+CREATE OR REPLACE VIEW recon.v_catch_input_not_reconstructed_catch_type_not_reported AS
+  SELECT id FROM catch WHERE input_type_id != 1 AND catch_type_id != 1;
+
+-- Layer is not 1, 2, or 3
+CREATE OR REPLACE VIEW recon.v_catch_layer_not_in_range AS
+  SELECT id FROM catch WHERE layer NOT IN (1,2,3);
+
+-- Catch amount is zero or negative
+CREATE OR REPLACE VIEW recon.v_catch_amount_zero_or_negative AS
+  SELECT id FROM catch WHERE amount <= 0;
