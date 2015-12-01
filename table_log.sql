a491a0f7f6939502c2d65ebda897accec0da4a4e
Rebel Scum <linht@vulcan.com>
2015-12-01 14:02:12 -0800

    Modified to add the log.adhoc_query table to store adhoc queries and track their usages.

--- table_log.sql
+++ table_log.sql
@@ -5,3 +5,17 @@ CREATE TABLE log.table_edits(
   notes text,
   created timestamp not null default now()
 );
+
+CREATE TABLE log.adhoc_query(
+  id serial primary key,
+  query text not null,
+  notes text,
+  is_active boolean not null default true,
+  created_by_auth_user_id int not null,
+  reviewed_by_auth_user_id int,
+  grantee_auth_user_id int[],
+  last_executed_by_auth_user_id int,
+  last_executed timestamp,
+  created timestamp not null default now(),
+  last_modified timestamp
+);
