37d729923b7c7b1d040952f1d7b4caa18eb5bb51
Rebel Scum <linht@vulcan.com>
2016-01-13 14:44:40 -0800

    Further query performance tweaks to improve response times of warning and error views on catch and raw_catch.

--- view_recon.sql
+++ view_recon.sql
@@ -4,47 +4,47 @@
    
 -- Year greater than the max year
 CREATE OR REPLACE VIEW recon.v_raw_catch_year_max AS
-SELECT id FROM raw_catch WHERE year > (SELECT max(year) FROM time);
+SELECT id FROM recon.raw_catch WHERE year > (SELECT max(year) FROM time);
 
 -- Original taxon name is not null
 CREATE OR REPLACE VIEW recon.v_raw_catch_original_taxon_not_null  AS
-SELECT id FROM raw_catch WHERE original_taxon_name IS NOT NULL; 
+SELECT id FROM recon.raw_catch WHERE original_taxon_name IS NOT NULL; 
 
 -- Original country fishing is not null
 CREATE OR REPLACE VIEW recon.v_raw_catch_original_country_fishing_not_null AS
-SELECT id FROM raw_catch WHERE original_country_fishing IS NOT NULL; 
+SELECT id FROM recon.raw_catch WHERE original_country_fishing IS NOT NULL; 
 
 -- Original sector is not null
 CREATE OR REPLACE VIEW recon.v_raw_catch_original_sector_not_null AS
-SELECT id FROM raw_catch WHERE original_sector IS NOT NULL; 
+SELECT id FROM recon.raw_catch WHERE original_sector IS NOT NULL; 
 
 -- Catch amount greater than 15e6 for Peru
 CREATE OR REPLACE VIEW recon.v_raw_catch_peru_catch_amount_greater_than_threshold AS
-SELECT id FROM raw_catch WHERE amount > 15e6 AND coalesce(eez_id, 0) = 604; 
+SELECT id FROM recon.raw_catch WHERE amount > 15e6 AND coalesce(eez_id, 0) = 604; 
 
 -- Catch amount greater than 5e6 for others
 CREATE OR REPLACE VIEW recon.v_raw_catch_amount_greater_than_threshold AS
-SELECT id FROM raw_catch WHERE amount > 5e6 AND coalesce(eez_id, 0) != 604;
+SELECT id FROM recon.raw_catch WHERE amount > 5e6 AND coalesce(eez_id, 0) != 604;
 
 -- FAO is 27 and ICES is null
 CREATE OR REPLACE VIEW recon.v_raw_catch_fao_27_ices_null AS
-SELECT id FROM raw_catch WHERE fao_area_id = 27 AND ices_area_id IS NULL; 
+SELECT id FROM recon.raw_catch WHERE fao_area_id = 27 AND ices_area_id IS NULL; 
 
 -- FAO is 21 and NAFO is null
 CREATE OR REPLACE VIEW recon.v_raw_catch_fao_21_nafo_null AS
-SELECT id FROM raw_catch WHERE fao_area_id = 21 AND nafo_division_id IS NULL; 
+SELECT id FROM recon.raw_catch WHERE fao_area_id = 21 AND nafo_division_id IS NULL; 
 
 -- Sector is subsistence and Layer is not 1
 CREATE OR REPLACE VIEW recon.v_raw_catch_subsistence_and_layer_not_1 AS
-SELECT id FROM raw_catch WHERE sector_type_id = 2 AND layer != 1; 
+SELECT id FROM recon.raw_catch WHERE sector_type_id = 2 AND layer != 1; 
 
 -- Layer is 2 or 3 and sector is not industrial
 CREATE OR REPLACE VIEW recon.v_raw_catch_layer_2_or_3_and_sector_not_industrial AS
-SELECT id FROM raw_catch WHERE layer IN (2,3) AND sector_type_id != 1; 
+SELECT id FROM recon.raw_catch WHERE layer IN (2,3) AND sector_type_id != 1; 
 
 -- Rare taxa should be excluded
 CREATE OR REPLACE VIEW recon.v_raw_catch_taxa_is_rare AS
-SELECT rc.id FROM raw_catch rc, rare_taxon rt WHERE rc.taxon_key = rt.taxon_key;
+SELECT rc.id FROM recon.raw_catch rc, rare_taxon rt WHERE rc.taxon_key = rt.taxon_key;
 
 --
 -- raw_catch errors (blocking)
@@ -57,7 +57,7 @@ WITH eez_fishing_entity AS (
   SELECT eez_id, is_home_eez_of_fishing_entity_id AS expected_fishing_entity FROM eez
 ) 
 SELECT rc.id 
-  FROM raw_catch rc 
+  FROM recon.raw_catch rc 
   LEFT JOIN eez_fishing_entity efe ON (rc.eez_id = efe.eez_id)
  WHERE coalesce(rc.eez_id, 0) !=0 
    AND fishing_entity_id != 0 
@@ -69,7 +69,7 @@ WITH eez_fishing_entity AS (
   SELECT eez.eez_id, eez.is_home_eez_of_fishing_entity_id AS expected_fishing_entity FROM eez
 )
 SELECT rc.id
-  FROM raw_catch rc
+  FROM recon.raw_catch rc
   LEFT JOIN eez_fishing_entity efe ON (rc.eez_id = efe.eez_id)
   LEFT JOIN layer3_taxon l3 ON (l3.taxon_key = rc.taxon_key)
  WHERE ((rc.eez_id IS DISTINCT FROM 0 AND rc.fishing_entity_id <> 0) OR l3.taxon_key IS NOT NULL) 
@@ -84,24 +84,27 @@ SELECT rc.id
 
 -- Input type is reconstructed and Catch type is reported landings
 CREATE OR REPLACE VIEW recon.v_raw_catch_input_reconstructed_catch_type_reported AS
-SELECT id FROM raw_catch WHERE coalesce(input_type_id, 0) = 1 AND catch_type_id = 1; 
+SELECT id FROM recon.raw_catch WHERE coalesce(input_type_id, 0) = 1 AND catch_type_id = 1; 
 
 -- Input type is not reconstructed and Catch type not reported landings
 CREATE OR REPLACE VIEW recon.v_raw_catch_input_not_reconstructed_catch_type_not_reported AS
-SELECT id FROM raw_catch WHERE coalesce(input_type_id, 0) != 1 AND catch_type_id != 1; 
+SELECT id FROM recon.raw_catch WHERE coalesce(input_type_id, 0) != 1 AND catch_type_id != 1; 
 
 -- Layer is not 1, 2, or 3
 CREATE OR REPLACE VIEW recon.v_raw_catch_layer_not_in_range AS
-SELECT id FROM raw_catch WHERE layer NOT IN (1,2,3); 
+SELECT id FROM recon.raw_catch WHERE layer NOT IN (1,2,3); 
 
 -- Catch amount is zero or negative
 CREATE OR REPLACE VIEW recon.v_raw_catch_amount_zero_or_negative AS
-SELECT id FROM raw_catch WHERE amount <= 0; 
+SELECT id FROM recon.raw_catch WHERE amount <= 0; 
 
 -- Lookup table mismatch
+-- NOTE: this view heavily relies on the index raw_catch_lookup_mismatch_idx for its performance, 
+--       so if you make any change to this view the index_recon.sql script should be reviewed as well to make sure the corresponding performance-related indexes are properly setup for this view.
+--
 CREATE OR REPLACE VIEW recon.v_raw_catch_lookup_mismatch AS
 SELECT rc.id 
-  FROM raw_catch rc        
+  FROM recon.raw_catch rc
  WHERE 0 = ANY(ARRAY[taxon_key, 
                      coalesce(original_taxon_name_id, -1), 
                      coalesce(original_fao_name_id, -1), 
@@ -113,16 +116,23 @@ SELECT rc.id
                      coalesce(input_type_id, -1), 
                      coalesce(reference_id, -1)]) 	
     OR eez_id IS NULL 	
-    OR "year" NOT IN (SELECT t.year FROM "time" t); 
+UNION
+SELECT rc.id 
+  FROM recon.raw_catch rc
+ WHERE rc.year NOT IN (SELECT t.year FROM master.time AS t);
+    
 
 -- Missing required field
+-- NOTE: this view heavily relies on the two indexes raw_catch_required_field_idx and raw_catch_taxon_name_is_null_idx for its performance, 
+--       so if you make any change to this view the index_recon.sql script should be reviewed as well to make sure the corresponding performance-related indexes are properly setup for this view.
+--
 CREATE OR REPLACE VIEW recon.v_raw_catch_missing_required_field AS
 SELECT id                
-  FROM raw_catch rc
+  FROM recon.raw_catch rc
  WHERE (fishing_entity || eez || fao_area || layer || sector || catch_type || "year" || amount || input_type) IS NULL
 UNION
 SELECT id                
-  FROM raw_catch rc
+  FROM recon.raw_catch rc
   LEFT JOIN distribution.taxon_distribution_substitute ts ON (ts.original_taxon_key = rc.taxon_key)
  WHERE ts.original_taxon_key IS NULL AND rc.taxon_name IS NULL
 ;
@@ -133,47 +143,47 @@ SELECT id
 
 -- Year greater than the max year
 CREATE OR REPLACE VIEW recon.v_catch_year_max AS
-  SELECT id FROM catch WHERE year > (SELECT max(year) FROM time);
+  SELECT id FROM recon.catch WHERE year > (SELECT max(year) FROM time);
 
 -- Original taxon name is not null
 CREATE OR REPLACE VIEW recon.v_catch_original_taxon_not_null  AS
-  SELECT id FROM catch WHERE original_taxon_name_id IS NOT NULL;
+  SELECT id FROM recon.catch WHERE original_taxon_name_id IS NOT NULL;
 
 -- Original country fishing is not null
 CREATE OR REPLACE VIEW recon.v_catch_original_country_fishing_not_null AS
-  SELECT id FROM catch WHERE original_country_fishing_id IS NOT NULL;
+  SELECT id FROM recon.catch WHERE original_country_fishing_id IS NOT NULL;
 
 -- Original sector is not null
 CREATE OR REPLACE VIEW recon.v_catch_original_sector_not_null AS
-  SELECT id FROM catch WHERE original_sector IS NOT NULL;
+  SELECT id FROM recon.catch WHERE original_sector IS NOT NULL;
 
 -- Catch amount greater than 15e6 for Peru
 CREATE OR REPLACE VIEW recon.v_catch_peru_catch_amount_greater_than_threshold AS
-  SELECT id FROM catch WHERE amount > 15e6 AND eez_id = 604;
+  SELECT id FROM recon.catch WHERE amount > 15e6 AND eez_id = 604;
 
 -- Catch amount greater than 5e6 for others
 CREATE OR REPLACE VIEW recon.v_catch_amount_greater_than_threshold AS
-  SELECT id FROM catch WHERE amount > 5e6 AND eez_id != 604;
+  SELECT id FROM recon.catch WHERE amount > 5e6 AND eez_id != 604;
 
 -- FAO is 27 and ICES is null
 CREATE OR REPLACE VIEW recon.v_catch_fao_27_ices_null AS
-  SELECT id FROM catch WHERE fao_area_id = 27 AND ices_area_id IS NULL;
+  SELECT id FROM recon.catch WHERE fao_area_id = 27 AND ices_area_id IS NULL;
 
 -- FAO is 21 and NAFO is null
 CREATE OR REPLACE VIEW recon.v_catch_fao_21_nafo_null AS
-  SELECT id FROM catch WHERE fao_area_id = 21 AND nafo_division_id IS NULL;
+  SELECT id FROM recon.catch WHERE fao_area_id = 21 AND nafo_division_id IS NULL;
 
 -- Sector is subsistence and Layer is not 1
 CREATE OR REPLACE VIEW recon.v_catch_subsistence_and_layer_not_1 AS
-  SELECT id FROM catch WHERE sector_type_id = 2 AND layer != 1;
+  SELECT id FROM recon.catch WHERE sector_type_id = 2 AND layer != 1;
 
 -- Layer is 2 or 3 and sector is not industrial
 CREATE OR REPLACE VIEW recon.v_catch_layer_2_or_3_and_sector_not_industrial AS
-  SELECT id FROM catch WHERE layer IN (2,3) AND sector_type_id != 1;
+  SELECT id FROM recon.catch WHERE layer IN (2,3) AND sector_type_id != 1;
 
 -- Rare taxa should be excluded
 CREATE OR REPLACE VIEW recon.v_catch_taxa_is_rare AS
-  SELECT c.id FROM catch c, rare_taxon rt WHERE c.taxon_key = rt.taxon_key;
+  SELECT c.id FROM recon.catch c, rare_taxon rt WHERE c.taxon_key = rt.taxon_key;
 
 --
 -- catch errors
@@ -184,7 +194,7 @@ CREATE OR REPLACE VIEW recon.v_catch_fishing_entity_and_eez_not_aligned AS
       SELECT eez.eez_id, eez.is_home_eez_of_fishing_entity_id AS expected_fishing_entity FROM eez
   )
   SELECT c.id
-    FROM catch c
+    FROM recon.catch c
     LEFT JOIN eez_fishing_entity efe ON (c.eez_id = efe.eez_id)
     LEFT JOIN layer3_taxon l3 ON (l3.taxon_key = c.taxon_key)
   WHERE ((c.eez_id <> 0 AND c.fishing_entity_id <> 0) OR l3.taxon_key IS NOT NULL)
@@ -199,19 +209,19 @@ CREATE OR REPLACE VIEW recon.v_catch_fishing_entity_and_eez_not_aligned AS
 
 -- Input type is reconstructed and Catch type is reported landings
 CREATE OR REPLACE VIEW recon.v_catch_input_reconstructed_catch_type_reported AS
-  SELECT id FROM catch WHERE input_type_id = 1 AND catch_type_id = 1;
+  SELECT id FROM recon.catch WHERE input_type_id = 1 AND catch_type_id = 1;
 
 -- Input type is not reconstructed and Catch type not reported landings
 CREATE OR REPLACE VIEW recon.v_catch_input_not_reconstructed_catch_type_not_reported AS
-  SELECT id FROM catch WHERE input_type_id != 1 AND catch_type_id != 1;
+  SELECT id FROM recon.catch WHERE input_type_id != 1 AND catch_type_id != 1;
 
 -- Layer is not 1, 2, or 3
 CREATE OR REPLACE VIEW recon.v_catch_layer_not_in_range AS
-  SELECT id FROM catch WHERE layer NOT IN (1,2,3);
+  SELECT id FROM recon.catch WHERE layer NOT IN (1,2,3);
 
 -- Catch amount is zero or negative
 CREATE OR REPLACE VIEW recon.v_catch_amount_zero_or_negative AS
-  SELECT id FROM catch WHERE amount <= 0;
+  SELECT id FROM recon.catch WHERE amount <= 0;
 
 --
 -- custom views
