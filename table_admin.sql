666564c8172aaa74b3c2dd75948fbdf390723225
Rebel Scum <linht@vulcan.com>
2016-01-14 13:55:52 -0800

    Added the admin.version table

--- table_admin.sql
+++ table_admin.sql
@@ -1,3 +1,16 @@
+CREATE TABLE admin.version (
+  id serial primary key,
+  name varchar(50),
+  major int not null,
+  minor int not null,
+  revision int,
+  is_active boolean not null default false,
+  released_to_qa timestamp,
+  released_to_staging timestamp,
+  released_to_production timestamp,
+  last_modified timestamp not null default now(),
+  description text
+);
 
 /*
 Forward declaration of this function to allow for its inclusion in each scripts below
@@ -5,9 +18,18 @@ Forward declaration of this function to allow for its inclusion in each scripts 
 --- Granting access to user is very important to enable insert/delete/update 
 --- operations on the tables
 
-CREATE OR REPLACE FUNCTION admin.grant_access() RETURNS void AS
-$body$
-  GRANT USAGE ON SCHEMA admin TO web_int, qc_int, recon_int, distribution_int, gis_int;
-$body$
-LANGUAGE sql
-SECURITY DEFINER;
+DO 
+$$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_proc WHERE proname = 'grant_access' LIMIT 1) THEN
+    CREATE OR REPLACE FUNCTION admin.grant_access() RETURNS void AS
+    $body$
+      GRANT USAGE ON SCHEMA admin TO web_int, qc_int, recon_int, distribution_int, gis_int;
+    $body$
+    LANGUAGE sql
+    SECURITY DEFINER;
+    
+    SELECT admin.grant_access();
+  END IF;
+END
+$$;
