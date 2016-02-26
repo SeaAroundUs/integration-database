fb472f717227fab2144fbdaf571a47df6d20383b
arash-t <a.tavakolie@fisheries.ubc.ca>
2016-02-25 16:23:23 -0800

    refactored to admin.grant_privilege()

--- grant.sql
+++ grant.sql
@@ -33,139 +33,51 @@ $body$
   -- For user web_int
   SELECT admin.grant_privilege('admin', 'web_int', false, false);
   SELECT admin.grant_privilege('log', 'web_int', false, false);
-  SELECT admin.grant_privilege('recon', 'web_int', true, false);
-  
-  GRANT USAGE ON SCHEMA recon TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA recon TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA recon TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO web_int;
-  
-  GRANT USAGE ON SCHEMA distribution TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA distribution TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA distribution TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA distribution TO web_int;
-  
-  GRANT USAGE ON SCHEMA log TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA log TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO web_int;
-  
-  GRANT USAGE ON SCHEMA catalog TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA catalog TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA catalog TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA catalog TO web_int;
-  
-  GRANT USAGE ON SCHEMA allocation TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA allocation TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA allocation TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA allocation TO web_int;
-  
-  GRANT USAGE ON SCHEMA geo TO web_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA geo TO web_int;
-  GRANT USAGE,SELECT,UPDATE ON ALL SEQUENCES IN SCHEMA geo TO web_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA geo TO web_int;
+  SELECT admin.grant_privilege('recon', 'web_int', false, false);
+  SELECT admin.grant_privilege('distribution', 'web_int', false, false);
+  SELECT admin.grant_privilege('catalog', 'web_int', false, false);
+  SELECT admin.grant_privilege('allocation', 'web_int', false, false);
+  SELECT admin.grant_privilege('geo', 'web_int', false, false);
   
   -- For user qc_int (user for UBC staff)
-  GRANT USAGE ON SCHEMA master TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA master TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA master TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA master TO qc_int;
-  
-  GRANT USAGE ON SCHEMA recon TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA recon TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA recon TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO qc_int;
-  
-  GRANT USAGE ON SCHEMA distribution TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA distribution TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA distribution TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA distribution TO qc_int;
-  
-  GRANT USAGE ON SCHEMA admin TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA admin TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA admin TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA admin TO qc_int;
-  
-  GRANT USAGE ON SCHEMA allocation TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA allocation TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA allocation TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA allocation TO qc_int;
-  
-  GRANT USAGE ON SCHEMA log TO qc_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO qc_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA log TO qc_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO qc_int;
-  
+  SELECT admin.grant_privilege('master', 'qc_int', false, false);
+  SELECT admin.grant_privilege('admin', 'qc_int', false, false);
+  SELECT admin.grant_privilege('log', 'qc_int', false, false);
+  SELECT admin.grant_privilege('recon', 'qc_int', false, false);
+  SELECT admin.grant_privilege('distribution', 'qc_int', false, false);
+  SELECT admin.grant_privilege('catalog', 'qc_int', true, false);
+  SELECT admin.grant_privilege('allocation', 'qc_int', false, false);
+  SELECT admin.grant_privilege('geo', 'qc_int', false, false);
+
   -- For user recon_int
-  GRANT USAGE ON SCHEMA admin TO recon_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA admin TO recon_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA admin TO recon_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA admin TO recon_int;
-  
-  GRANT USAGE ON SCHEMA master TO recon_int;
-  GRANT SELECT,INSERT,UPDATE,DELETE,REFERENCES ON ALL TABLES IN SCHEMA master TO recon_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA master TO recon_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA master TO recon_int;
-  
-  GRANT USAGE ON SCHEMA log TO recon_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO recon_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA log TO recon_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO recon_int;
-  
-  GRANT USAGE ON SCHEMA recon TO recon_int;
-  GRANT ALL ON ALL TABLES IN SCHEMA recon TO recon_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA recon TO recon_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO recon_int;
-  
-  GRANT USAGE ON SCHEMA log TO recon_int;
-  GRANT ALL ON ALL TABLES IN SCHEMA log TO recon_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA log TO recon_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO recon_int;
-  
-  GRANT USAGE ON SCHEMA distribution TO recon_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA distribution TO recon_int;
+  SELECT admin.grant_privilege('master', 'recon_int', true, true);
+  SELECT admin.grant_privilege('admin', 'recon_int', false, false);
+  SELECT admin.grant_privilege('log', 'recon_int', true, true);
+  SELECT admin.grant_privilege('recon', 'recon_int', true, true);
+  SELECT admin.grant_privilege('distribution', 'recon_int', false, false);
+  SELECT admin.grant_privilege('catalog', 'recon_int', true, false);
+  SELECT admin.grant_privilege('allocation', 'recon_int', false, false);
+  SELECT admin.grant_privilege('geo', 'qc_int', false, false);
+
+  -- more granular access for the following tables:
   GRANT INSERT,UPDATE,SELECT,DELETE,REFERENCES ON distribution.taxon_habitat TO recon_int;
   GRANT INSERT,UPDATE,SELECT,DELETE,REFERENCES ON distribution.taxon_distribution_substitute TO recon_int;
   
   -- For user distribution_int
-  GRANT USAGE ON SCHEMA admin TO distribution_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA admin TO distribution_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA admin TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA admin TO distribution_int;
-  
-  GRANT USAGE ON SCHEMA master TO distribution_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA master TO distribution_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA master TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA master TO distribution_int;
-  
-  GRANT USAGE ON SCHEMA log TO distribution_int;
-  GRANT INSERT,UPDATE,SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO distribution_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA log TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO distribution_int;
-  
-  GRANT USAGE ON SCHEMA distribution TO distribution_int;
-  GRANT ALL ON ALL TABLES IN SCHEMA distribution TO distribution_int;
-  GRANT ALL ON ALL SEQUENCES IN SCHEMA distribution TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA distribution TO distribution_int;
-  
-  GRANT USAGE ON SCHEMA recon TO distribution_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA recon TO distribution_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA recon TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA recon TO distribution_int;
-
-  GRANT USAGE ON SCHEMA log TO distribution_int;
-  GRANT SELECT,REFERENCES ON ALL TABLES IN SCHEMA log TO distribution_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA log TO distribution_int;
-  GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA log TO distribution_int;
-  
+  SELECT admin.grant_privilege('master', 'distribution_int', false, false);
+  SELECT admin.grant_privilege('admin', 'distribution_int', false, false);
+  SELECT admin.grant_privilege('log', 'distribution_int', false, false);
+  SELECT admin.grant_privilege('recon', 'distribution_int', false, false);
+  SELECT admin.grant_privilege('distribution', 'distribution_int', true, true);
+  SELECT admin.grant_privilege('geo', 'distribution_int', false, false);
+   
   -- For user gis_int
-  GRANT USAGE ON SCHEMA distribution TO gis_int;
-  GRANT INSERT,UPDATE,SELECT,DELETE,REFERENCES ON distribution.taxon_extent TO gis_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA distribution TO gis_int;
-  
-  GRANT USAGE ON SCHEMA geo TO gis_int;
-  GRANT INSERT,UPDATE,SELECT,DELETE,REFERENCES ON ALL TABLES IN SCHEMA geo TO gis_int;
-  GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA geo TO gis_int;
+  SELECT admin.grant_privilege('master', 'gis_int', false, false);
+  SELECT admin.grant_privilege('admin', 'gis_int', false, false);
+  SELECT admin.grant_privilege('log', 'gis_int', false, false);
+  SELECT admin.grant_privilege('recon', 'gis_int', false, false);
+  SELECT admin.grant_privilege('distribution', 'gis_int', false, false);
+  SELECT admin.grant_privilege('geo', 'gis_int', true, true);
 $body$
 LANGUAGE sql
 SECURITY DEFINER;
