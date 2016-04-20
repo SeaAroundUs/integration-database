eec5a048fa26c1459fbdf2506e7d3b5ccba65935
Rebel Scum <linht@vulcan.com>
2016-04-20 11:23:19 -0700

    Fixed typo in the schema name for the django_migrations table.

--- data_dump/dump_schema.bat
+++ data_dump/dump_schema.bat
@@ -11,6 +11,6 @@ IF [%3]==[] (
   SET DbPort=%3
 )
 echo Password for user sau_int
-pg_dump -h %DbServer% -p %DbPort% -f %1.schema -T web.django_migrations -Fc -a -E UTF8 -U sau_int -n %1 %4 %5 %6 %7 %8 %9 sau_int 
+pg_dump -h %DbServer% -p %DbPort% -f %1.schema -T recon.django_migrations -Fc -a -E UTF8 -U sau_int -n %1 %4 %5 %6 %7 %8 %9 sau_int 
 @ECHO OFF
 
