11303d1c54492e074ffc78c6a51362cfbcc8b7b1
Rebel Scum <linht@vulcan.com>
2016-04-20 09:03:35 -0700

    Added trigger to taxon_distribution table to prevent insertion of records with relative_abundance = 0.

--- trigger_distribution.sql
+++ trigger_distribution.sql
@@ -19,3 +19,20 @@ LANGUAGE plpgsql;
 CREATE TRIGGER taxon_extent_before_insert_trigger BEFORE INSERT
             ON distribution.taxon_extent
   FOR EACH ROW EXECUTE PROCEDURE distribution.taxon_extent_insert_trigger_handler();
+
+CREATE OR REPLACE FUNCTION distribution.taxon_distribution_insert_trigger_handler() RETURNS TRIGGER AS
+$body$
+BEGIN
+  -- Ignore the insert if the record has 0 relative abundance
+  IF NEW.relative_abundance = 0.0 THEN
+    RETURN NULL;
+  ELSE
+    RETURN NEW;
+  END IF;
+END;
+$body$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER taxon_distribution_before_insert_trigger BEFORE INSERT
+            ON distribution.taxon_distribution
+  FOR EACH ROW EXECUTE PROCEDURE distribution.taxon_distribution_insert_trigger_handler();
