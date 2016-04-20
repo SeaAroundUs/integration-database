b8d22211fcdeba5c71b29326a345aaed8bfeb2e2
Rebel Scum <linht@vulcan.com>
2016-04-20 11:20:21 -0700

    Added optional parameter to allow for more flexible options that the user may want to add. For instance, table exclusion list.

--- data_dump/dump_schema.sh
+++ data_dump/dump_schema.sh
@@ -12,4 +12,4 @@ else
   DbPort=$3
 fi
 echo Password for user sau_int
-pg_dump -h $DbHost -p $DbPort -f $1.schema -T web.django_migrations -Fc -a -E UTF8 -U sau_int -n $1 sau_int
+pg_dump -h $DbHost -p $DbPort -f $1.schema -T web.django_migrations -Fc -a -E UTF8 -U sau_int -n $1 $4 $5 $6 $7 $8 $9 sau_int
