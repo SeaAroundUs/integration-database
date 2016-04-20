c4c78959a84da0389a73502a51329017860b0a9e
Rebel Scum <linht@vulcan.com>
2016-04-20 09:22:08 -0700

    Updated user paths to incorporate couple new schemas that were added recently.

--- set_users_search_path.sql
+++ set_users_search_path.sql
@@ -1,6 +1,6 @@
-ALTER USER sau_int SET search_path TO admin, master, recon, distribution, log, tiger, topology, tiger_data, public;
-ALTER USER web_int SET search_path TO master, recon, distribution, log, admin, tiger, topology, tiger_data, public;
-ALTER USER recon_int SET search_path TO recon, log, master, admin, distribution, tiger, topology, tiger_data, public;
-ALTER USER distribution_int SET search_path TO distribution, master, admin, recon, log, tiger, topology, tiger_data, public;
-ALTER USER qc_int SET search_path TO master, recon, distribution, log, admin, public;
-ALTER USER gis_int SET search_path TO distribution, log, admin, public;
+ALTER USER sau_int SET search_path TO admin, master, recon, distribution, geo, log, tiger, topology, tiger_data, public;
+ALTER USER web_int SET search_path TO master, recon, distribution, geo, log, admin, tiger, topology, tiger_data, public;
+ALTER USER recon_int SET search_path TO recon, log, master, admin, distribution, geo, tiger, topology, tiger_data, public;
+ALTER USER distribution_int SET search_path TO distribution, master, admin, recon, geo, log, tiger, topology, tiger_data, public;
+ALTER USER qc_int SET search_path TO master, recon, distribution, geo, log, admin, public;
+ALTER USER gis_int SET search_path TO distribution, geo, log, admin, public;
