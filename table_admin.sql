71911824cc2d66614e84bf3c5c7c5fbbf79304cf
Rebel Scum <linht@vulcan.com>
2016-04-20 09:08:42 -0700

    Added the datatransfer_tables to maintain a list of tables to sync with other databases.

--- table_admin.sql
+++ table_admin.sql
@@ -12,6 +12,21 @@ CREATE TABLE admin.version (
   description text
 );
 
+/* For transfering of external data to the integration db from either postgres or SQL Server sources */
+CREATE TABLE admin.datatransfer_tables(
+  id SERIAL PRIMARY KEY,
+  source_database_name VARCHAR(256),
+  source_table_name VARCHAR(256),
+  source_key_column VARCHAR(256),
+  source_where_clause TEXT,
+  target_schema_name VARCHAR(256),
+  target_table_name VARCHAR(256),
+  target_excluded_columns TEXT[],
+  number_of_threads INT NOT NULL DEFAULT 1,
+  last_transferred TIMESTAMP,
+  last_transfer_success BOOLEAN
+);
+
 /*
 Forward declaration of this function to allow for its inclusion in each scripts below
 */
