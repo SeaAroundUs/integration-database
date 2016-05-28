c3e88884a077997627d3b0edaf45d98c3cea360c
Rebel Scum <linht@vulcan.com>
2016-05-27 17:49:41 -0700

    Updated to add the data_raw_layer3 to the list of tables to pull from Merlin.

--- populate_admin.sql
+++ populate_admin.sql
@@ -3,5 +3,5 @@ TRUNCATE TABLE admin.datatransfer_tables;
 INSERT INTO admin.datatransfer_tables(source_database_name, source_table_name, source_where_clause, target_schema_name, target_table_name, target_excluded_columns)
 VALUES
  ('Merlin', 'Log_Import_Raw', NULL, 'allocation', 'log_import_raw', '{}'::TEXT[]) 
---,('Merlin', 'AreaInvisible', NULL, 'web', 'area_invisible', '{}'::TEXT[])
+,('Merlin', 'DataRaw_Layer3', NULL, 'allocation', 'data_raw_layer3', '{}'::TEXT[])
 ;
