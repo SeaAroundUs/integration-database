15a2f6cb0219044cf8a47d6d2e055284022b6281
Rebel Scum <linht@vulcan.com>
2016-06-14 18:12:40 -0700

    Modified to add the source_select_clause to the admin.datatransfer_tables table

--- populate_admin.sql
+++ populate_admin.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE admin.datatransfer_tables;
 
-INSERT INTO admin.datatransfer_tables(source_database_name, source_table_name, source_where_clause, target_schema_name, target_table_name, target_excluded_columns)
+INSERT INTO admin.datatransfer_tables(source_database_name, source_table_name, source_select_clause, source_where_clause, target_schema_name, target_table_name, target_excluded_columns)
 VALUES
- ('Merlin', 'Log_Import_Raw', NULL, 'allocation', 'log_import_raw', '{}'::TEXT[]) 
-,('Merlin', 'DataRaw_Layer3', NULL, 'allocation', 'data_raw_layer3', '{}'::TEXT[])
+ ('Merlin', 'Log_Import_Raw', '*', NULL, 'allocation', 'log_import_raw', '{}'::TEXT[]) 
+,('Merlin', 'DataRaw_Layer3', '*', NULL, 'allocation', 'data_raw_layer3', '{}'::TEXT[])
 ;
