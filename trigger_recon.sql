e988a61b6a41bfeef90fb6e43bedaa74448c5051
Rebel Scum <linht@vulcan.com>
2016-07-25 11:53:18 -0700

    Updated to truncate trailing ".0"

--- trigger_recon.sql
+++ trigger_recon.sql
@@ -9,6 +9,10 @@ BEGIN
      NEW.row_id := nextval('recon.reference_row_id_seq'); 
   END IF;
   
+  IF NEW.reference_id::TEXT LIKE '%.0' THEN
+    NEW.reference_id := NEW.reference_id::INT;
+  END IF;
+  
   RETURN NEW;
 END;
 $body$
