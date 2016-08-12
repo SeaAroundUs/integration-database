defcce72000dd352d578547b1845d48cb11cc674
Rebel Scum <linht@vulcan.com>
2016-08-11 21:04:13 -0700

    Updated to add triggers to the taxon_habitat table to auto-calculate the effective_d and habitat_index columns.

--- trigger_distribution.sql
+++ trigger_distribution.sql
@@ -41,3 +41,50 @@ LANGUAGE plpgsql;
 CREATE TRIGGER taxon_distribution_before_insert_trigger BEFORE INSERT
             ON distribution.taxon_distribution
   FOR EACH ROW EXECUTE PROCEDURE distribution.taxon_distribution_insert_trigger_handler();
+
+------
+
+CREATE OR REPLACE FUNCTION distribution.taxon_habitat_insert_update_trigger_handler() RETURNS TRIGGER AS
+$body$
+BEGIN
+  IF TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND TG_NAME = 'taxon_habitat_before_update_trigger_for_hdi') THEN
+    NEW.habitat_diversity_index := 
+      (SELECT COUNT(*)/5.0 
+         FROM unnest(ARRAY[COALESCE(NEW.estuaries, 0) > 1,
+                           COALESCE(NEW.coral, 0) > 1,
+                           COALESCE(NEW.sea_grass, 0) > 1,
+                           COALESCE(NEW.sea_mount, 0) > 1,
+                           COALESCE(NEW.others, 0) > 1]) AS t(f)  
+       WHERE t.f
+      );
+  END IF;
+                    
+  IF NEW.sl_max IS NOT NULL AND NEW.habitat_diversity_index IS NOT NULL THEN
+    NEW.effective_distance := distribution.effective_distance(NEW.sl_max, NEW.habitat_diversity_index);
+  END IF;
+                              
+  RETURN NEW;
+END;
+$body$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER taxon_habitat_before_insert_trigger 
+BEFORE INSERT ON distribution.taxon_habitat
+FOR EACH ROW 
+  EXECUTE PROCEDURE distribution.taxon_habitat_insert_update_trigger_handler();
+
+CREATE TRIGGER taxon_habitat_before_update_trigger_for_hdi 
+BEFORE UPDATE ON distribution.taxon_habitat
+FOR EACH ROW
+WHEN (OLD.estuaries IS DISTINCT FROM NEW.estuaries OR 
+      OLD.coral IS DISTINCT FROM NEW.coral OR 
+      OLD.sea_grass IS DISTINCT FROM NEW.sea_grass OR 
+      OLD.sea_mount IS DISTINCT FROM NEW.sea_mount OR 
+      OLD.others IS DISTINCT FROM NEW.others)    
+EXECUTE PROCEDURE distribution.taxon_habitat_insert_update_trigger_handler();
+
+CREATE TRIGGER taxon_habitat_before_update_trigger_for_ed
+BEFORE UPDATE ON distribution.taxon_habitat
+FOR EACH ROW
+WHEN (OLD.sl_max IS DISTINCT FROM NEW.sl_max OR OLD.habitat_diversity_index IS DISTINCT FROM NEW.habitat_diversity_index)    
+EXECUTE PROCEDURE distribution.taxon_habitat_insert_update_trigger_handler();
