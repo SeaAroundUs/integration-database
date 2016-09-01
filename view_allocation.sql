5040c316fa9a9a0ba4cc52b6e70d41b527a0fe94
arash-t <a.tavakolie@fisheries.ubc.ca>
2016-09-01 15:10:33 -0700

    added a new view to the allocation schema

--- view_allocation.sql
+++ view_allocation.sql
@@ -199,6 +199,28 @@ CREATE MATERIALIZED VIEW allocation.allocation_error_log AS
     JOIN recon.catch c ON (c.id = l.original_row_id)
     JOIN master.sector_type st ON (st.sector_type_id = c.sector_type_id);
 
+CREATE OR REPLACE VIEW allocation.v_recon_catch_for_export AS SELECT c.raw_catch_id,
+    c.layer,
+    c.fishing_entity_id,
+    c.eez_id,
+    c.fao_area_id,
+    c.year,
+    c.taxon_key,
+    c.amount,
+    st.name AS sector,
+    c.catch_type_id,
+    c.reporting_status_id,
+    it.name AS input,
+    ia.ices_area,
+    c.ccamlr_area,
+    na.nafo_division
+   FROM (((((recon.catch c
+     JOIN master."time" y ON (((y.year = c.year) AND y.is_used_for_allocation)))
+     JOIN master.sector_type st ON ((st.sector_type_id = c.sector_type_id)))
+     JOIN master.input_type it ON ((it.input_type_id = c.input_type_id)))
+     LEFT JOIN recon.ices_area ia ON ((ia.ices_area_id = c.ices_area_id)))
+     LEFT JOIN recon.nafo na ON ((na.nafo_division_id = c.nafo_division_id)));
+
 /*
 The command below should be maintained as the last command in this entire script.
 */
