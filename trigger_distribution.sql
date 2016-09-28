d92db5283e0e73b6aab7c6f767035e373782d17e
Rebel Scum <linht@vulcan.com>
2016-09-28 12:06:01 -0700

    Updated to add a new trigger to sync the sl_max, min/max_depth and lat_north/south params of master.taxon with those in the HI table.

--- trigger_distribution.sql
+++ trigger_distribution.sql
@@ -63,6 +63,16 @@ BEGIN
     NEW.effective_distance := distribution.effective_distance(NEW.sl_max, NEW.habitat_diversity_index);
   END IF;
                               
+  IF TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND TG_NAME = 'taxon_habitat_before_update_trigger_for_taxon_sync') THEN
+    UPDATE master.taxon t
+       SET sl_max = NEW.sl_max,
+           lat_north = NEW.lat_north, 
+           lat_south = NEW.lat_south,            
+           min_depth = NEW.min_depth, 
+           max_depth = NEW.max_depth
+     WHERE t.taxon_key = NEW.taxon_key;
+  END IF;
+  
   RETURN NEW;
 END;
 $body$
@@ -87,4 +97,12 @@ CREATE TRIGGER taxon_habitat_before_update_trigger_for_ed
 BEFORE UPDATE ON distribution.taxon_habitat
 FOR EACH ROW
 WHEN (OLD.sl_max IS DISTINCT FROM NEW.sl_max OR OLD.habitat_diversity_index IS DISTINCT FROM NEW.habitat_diversity_index)    
+EXECUTE PROCEDURE distribution.taxon_habitat_insert_update_trigger_handler();
+
+CREATE TRIGGER taxon_habitat_before_update_trigger_for_taxon_sync
+BEFORE UPDATE ON distribution.taxon_habitat
+FOR EACH ROW
+WHEN (OLD.min_depth IS DISTINCT FROM NEW.min_depth OR OLD.max_depth IS DISTINCT FROM NEW.max_depth OR 
+      OLD.lat_north IS DISTINCT FROM NEW.lat_north OR OLD.lat_south IS DISTINCT FROM NEW.lat_south OR
+      OLD.sl_max IS DISTINCT FROM NEW.sl_max)    
 EXECUTE PROCEDURE distribution.taxon_habitat_insert_update_trigger_handler();
