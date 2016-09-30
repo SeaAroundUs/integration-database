f5fd41de2602075430012410c404a2becdc7a248
Rebel Scum <linht@vulcan.com>
2016-09-30 14:48:21 -0700

    Added validation rule refresh for each of the changed views as any previously generated validation results for these views are now in question as to their validity.

--- migration/2016-09-30_Depth_Lat_HI_Flag.sql
+++ migration/2016-09-30_Depth_Lat_HI_Flag.sql
@@ -13,22 +13,30 @@ create or replace view recon.v_distribution_taxon_lat_north_null as
     join master.taxon t on (t.taxon_key = h.taxon_key and not t.is_retired)
    where h.lat_north is null;
   
+select recon.refresh_validation_result_partition('v_distribution_taxon_lat_north_null');
+
 create or replace view recon.v_distribution_taxon_lat_south_null as
   select h.taxon_key as id 
     from distribution.taxon_habitat h
     join master.taxon t on (t.taxon_key = h.taxon_key and not t.is_retired)
    where h.lat_south is null;
   
+select recon.refresh_validation_result_partition('v_distribution_taxon_lat_south_null');
+
 create or replace view recon.v_distribution_taxon_min_depth_null as
   select h.taxon_key as id 
     from distribution.taxon_habitat h
     join master.taxon t on (t.taxon_key = h.taxon_key and not t.is_retired)
    where h.min_depth is null;
   
+select recon.refresh_validation_result_partition('v_distribution_taxon_min_depth_null');
+
 create or replace view recon.v_distribution_taxon_max_depth_null as
   select h.taxon_key as id 
     from distribution.taxon_habitat h
     join master.taxon t on (t.taxon_key = h.taxon_key and not t.is_retired)
    where h.max_depth is null;
-   
+
+select recon.refresh_validation_result_partition('v_distribution_taxon_max_depth_null');
+
 select admin.grant_access();
